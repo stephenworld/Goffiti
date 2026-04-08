ff3f943442528034790a87c3db88ee3ed800fde3
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-04-08 15:31:51 +0100

    feat: added new feature to handle color input

---
+++ cmd/GenerateAsciiArtColor.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func GenerateAsciiArtColor(SUB, STR, COLOR string) string {
+	font, err := os.ReadFile("banners/standard.txt")
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fontLines := strings.Split(string(font), "\n")
+	strLines := strings.Split(STR, "\\n")
+
+	var ART strings.Builder
+
+	for _, word := range strLines {
+		if word == "" {
+			ART.WriteString("\n")
+			continue
+		}
+		for idx := range 8 {
+			for _, char := range word {
+				charLine := GetCharLine(char, fontLines)
+
+				ART.WriteString(charLine[idx])
+			}
+			ART.WriteString("\n")
+		}
+	}
+
+	return ART.String()
+}
