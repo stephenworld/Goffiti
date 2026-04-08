a7f12b6967cf27434246300e05f36b60b3f09e9f
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-04-08 15:33:25 +0100

    WIP: currently updating

--- main.go
+++ main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"goffiti/utils"
+	"goffiti/cmd"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Error: Expected atleast 2 arguments")
-		return
+
+	// Optional Flags
+	align := flag.String("align", "", "Align STRING")
+	color := flag.String("color", "", "Apply color to STRING")
+	output := flag.String("output", "", "Add content to the output file")
+
+	// Parse Flag contents
+	flag.Parse()
+
+	// Stores arguments that are not flags
+	arguments := flag.Args()
+
+	art := ""
+
+	if len(arguments) == 1 {
+		art = cmd.GenerateAsciiArt(arguments[0])
+		fmt.Print(art)
 	}
 
-	input := os.Args[1]
+	if len(arguments) == 2 && len(os.Args) == 3 {
+		art := cmd.GenerateAsciiArtFS(arguments[0], arguments[1])
+		fmt.Println("Goffiti FS Output Start")
+		fmt.Print(art)
+		fmt.Println("Goffiti FS Output End")
+	}
 
-	output := utils.AsciiArt(input)
-	fmt.Print(output)
+	if *align != "" {
+		if len(arguments) == 2 {
+			FSArt := cmd.GenerateAsciiArtFS(arguments[0], arguments[1])
+			art = cmd.GenerateAsciiArtJustify(FSArt, *align, cmd.GetTerminalWidth())
+			fmt.Println(art)
+		}
+		art = cmd.GenerateAsciiArtJustify(art, *align, cmd.GetTerminalWidth())
+		fmt.Println(art)
+	}
+
+	if *color != "" {
+	}
+
+	if *output != "" {
+		if len(arguments) == 2 {
+			art := cmd.GenerateAsciiArtFS(arguments[0], arguments[1])
+			os.WriteFile(*output, []byte(art), 0644)
+		}
+		art = cmd.GenerateAsciiArt(arguments[0])
+		os.WriteFile(*output, []byte(art), 0644)
+	}
 }
