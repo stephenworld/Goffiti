c64035faa986d92a94db4da1cedeac658922cd6d
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-04-28 12:24:47 +0100

    updated the webpage

--- web/index.js
+++ web/index.js
@@ -1,17 +1,17 @@
-function handleCopy(output){
-    if (output == ""){
-       alert("Nothing to copy. Please generate your art first.")
-    }else{
+function handleCopy(output) {
+    if (output == "") {
+        alert("Nothing to copy. Please generate your art first.")
+    } else {
         navigator.clipboard.writeText(output)
         alert("Art Successfully copied to clipboard")
     }
 }
 
-function handleDownload(output){
-    if (output == ""){
-       alert("No content available to download. Please submit the form.")
-    }else{
-        downloadFile(String(output),"art.txt","text/plain")
+function handleDownload(output) {
+    if (output == "") {
+        alert("No content available to download. Please submit the form.")
+    } else {
+        downloadFile(String(output), "art.txt", "text/plain")
     }
 }
 
